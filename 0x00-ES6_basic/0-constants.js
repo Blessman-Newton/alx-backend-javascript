d26c819782118db4012903551bcfd867cb2557ed
Blessman-Newton <blessmannewton0@gmail.com>
2023-11-27 22:48:35 +0000

    Add 0-constant file

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,1 +1,13 @@
-''
+export function taskFirst(){
+    const task = 'I prefer cost when I can.';
+    return task;
+}
+export function getlast(){
+    return 'is okay'
+}
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getlast();
+
+    return combination;
+}
